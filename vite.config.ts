ce02d94dc1b637b64d7f1d6e4ede300334504ec7
astafievK <AstafevK@yandex.ru>
2025-02-05 22:59:58 +0300

    css media formatted

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
+// Подключаем postcss напрямую, если конфиг лежит рядом
 export default defineConfig({
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
         { src: 'Dockerfile', dest: '' },
-      ]
-    })
+      ],
+    }),
   ]
 });
